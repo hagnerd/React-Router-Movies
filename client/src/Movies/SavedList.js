9275079111b026dc2845b2172d382f9cea55b695
Matt Hagner <web@matthagner.com>
2019-06-19 19:52:50 -0500

    Turn saved movies into links

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -11,9 +11,13 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span key={movie.id} className="saved-movie">
+          <Link
+            to={`/movies/${movie.id}`}
+            key={movie.id}
+            className="saved-movie"
+          >
             {movie.title}
-          </span>
+          </Link>
         ))}
         <Link to="/" className="home-button">
           Home
